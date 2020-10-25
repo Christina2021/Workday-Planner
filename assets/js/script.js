d67770da46eb3af76903e82578e2eb93a9beb9f4
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-10-25 12:25:34 -0700

    Initial Pseudocode for JS file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Add current day/time to the jumbotron
+
+//append timeblocks between 8-5
+//must add classes, hours (parseInt for later), maybe attributes
+//Middle column must be a textarea
+
+//compare times with current time to to add past/present/future class
+
+//save button needs to save to local storage
