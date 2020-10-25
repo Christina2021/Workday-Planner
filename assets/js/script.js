5a7a0403de31c68312ad5587938137324fe321b0
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-10-25 15:52:46 -0700

    Data added to local storage.  Also, data will pull from local storage and put in the correct textareas.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 //Variables
 const currentMoment = moment();
 const currentHour = moment().startOf('hour');
+//Retreiving local Storage
+let userData;
+//Saving to local Storage
+let userDataID;
+let userTextContent;
+let textObj;
+let allText;
 
 //Add current day/time to the jumbotron
 $('#currentDay').append(currentMoment.format('dddd[:] LL[ at] LT'));
@@ -17,28 +24,71 @@ for (j = 8; j < 18; j++){
     //Adds column with hour
     $(`#${j}`).append(`<div class="col-1 pt-3 hour">${moment(j, ["HH"]).format("h A")}</div>`);
     //Adds column with textbox
-    $(`#${j}`).append(`<div class="col-10 pl-0 user-text${j}"><textarea name="" id="" cols="" rows=""></textarea></div>`);
+    $(`#${j}`).append(`<div class="col-10 pl-0" id="user-text${j}"><textarea></textarea></div>`);
     //Adds column with save button
-    $(`#${j}`).append(`<div class="col-1 saveBtn"><i class="fas fa-save fa-lg save-icon"></i></div>`);
+    $(`#${j}`).append(`<div class="col-1 saveBtn"><i class="fas fa-save save-icon" for="user-text${j}" type="submit"></i></div>`);
 
 
-   //Add past/present/future classes    
+   //Add past/present/future classes by comparing current hour with row's hour
     let rowHour = moment().set('hour', j).startOf("hour");
-    console.log(rowHour);
-    console.log(currentHour);
     if(rowHour.isBefore(currentHour)){
-        $(`.user-text${j}`).addClass('past');
+        $(`#user-text${j}`).addClass('past');
     } else if(rowHour.isSame(currentHour)){
-        $(`.user-text${j}`).addClass('present');
+        $(`#user-text${j}`).addClass('present');
     } else if(rowHour.isAfter(currentHour)){
-        $(`.user-text${j}`).addClass('future');
+        $(`#user-text${j}`).addClass('future');
     };
 
 };
 
-//must add classes, hours (parseInt for later), maybe attributes
-//Middle column must be a textarea
+//Pulls data from local storage to add to container
+function retrieveUserData(){
 
-//compare times with current time to to add past/present/future class
+    userData = JSON.parse(localStorage.getItem("usersCurrentPlanner"));
 
-//save button needs to save to local storage
+    //If there is no user Data
+    if(!userData){
+        return;
+    }else{
+        //Check each item in local Storage and stick it in the appropriate textareas
+        for(k = 0; k < userData.length; k++){
+            if(userData[k].TextID) {
+                $(`#${userData[k].TextID} textarea`).val(userData[k].Content);
+            }else {
+                return;
+            };
+        };
+    };
+};
+
+retrieveUserData();
+
+//Save button needs to save to local storage
+$(`.save-icon`).click(function(e) {
+
+    //To pull ID of specific textarea
+    userDataID = $(this).attr('for');
+    //To show content listed in specific textarea
+    userTextContent = $(`#${userDataID} textarea`).val();
+
+    if(!userTextContent){
+        localStorage.removeItem(`${userDataID}`);
+        return;
+    } else {
+        alert("works");
+    };
+
+    //Put user's text into an object
+    textObj = {TextID: userDataID, Content: userTextContent};
+    //Get previous text items
+    allText = JSON.parse(localStorage.getItem("usersCurrentPlanner"))
+    //If nothing is currently in local Storage
+    if(!allText){
+        allText = [];
+        allText[0] = textObj;
+    } else {
+        allText.push(textObj);
+    };
+    //Convert object to string and put in local Storage
+    localStorage.setItem("usersCurrentPlanner", JSON.stringify(allText));
+});
